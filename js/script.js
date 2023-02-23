f94226e388e6e6b21505c86483e97cfaa1c5be5d
Preentrega1Pereyra <francoadolfopereyra@gmail.com>
2023-02-23 18:05:39 -0300

    estoy tratando pero me cuesta mucho, y es lo mas facil en teoria, siento sinceramente que no sirvo para esto

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// let savedName = "River2023";
+
+// function login() {
+//     let ingresar = false;
+//     for (let i = 2; i >= 0; i--) {
+//         let userName = prompt("Ingresa tu userName" + (i + 1));
+//         if(userName === savedName) {
+//             alert("UserName correcto")
+//             ingresar = true
+//             break;
+
+//         }
+
+//     }
+// }
+// let pastilla = "negra";
+
+// if (pastilla == "rojo") {
+//     console.log("Salir de la Matrix");
+// } else if (pastilla == "azul") {
+//     consola.log("Ignorancia");
+// } else {
+//     console.log("Elegiste un color inválido");
+// }
+// let edadUsuario = parseInt(prompt('Ingresá tu edad'));
+// sea ​​edadPermitida = 18;
+// if (edadUsuario >= edadPermitida) {
+//     console.log('Pase usted, aqui tiene su birrita')
+// } else {
+//     console.log('Raja de aca enano');
+// } */
+// esto no lo puede hacer andar.. no se en que me equivoco
+// let claveGuardada = "2023"
+// function login(claveGuardada) {
+//     let ingresar = false;
+//     for (let i = 2; i >= 0; i--) {
+//         let claveIngresada = prompt("Ingresa tu clave. Tenes" + (i + 2) + "intentos" );
+//         if (claveGuardada === claveIngresada) {
+//             alert(Ingreso exitoso);
+//             ingresar = true;
+//             break;
+//             else{
+//                 alert(error)
+//             }
+//         }
+//     }
+//     return ingresar
+// }
+// lo que quiero hacer es un programa con datos de mi laburo para que ingresemos el ipem y el año y nos salgan las resolusiones y cohorte que corresponde
+// let claveGuardada = "2023";
+
+        
+    
+// let ipem = 0;
+// let anio = 0;
+// prompt("ingrese Número de IPEM");
+// prompt("ingrese cohorte");
